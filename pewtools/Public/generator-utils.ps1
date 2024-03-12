bb864acbb5f33308707f22778a98a93a052aac50
Kevin Kvissberg <Kevin.Kvissberg@gmail.com>
2024-03-12 21:27:55 +0100

    New-PasswordUsingWords

--- pewtools/Public/generator-utils.ps1
+++ pewtools/Public/generator-utils.ps1
@@ -68,9 +68,10 @@ Function New-Password {
         throw "OnlyLowercase and OnlyUppercase cannot be used together."
     }
 
-
+    # Declare variables
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+/.,<>?;:[]{}-="
 
+    # Remove characters based on constraints
     if ($NoSpecial) {
         $chars = $chars -replace "[\W]", ""
         $chars = $chars -replace "[-_]", ""
@@ -91,11 +92,69 @@ Function New-Password {
         $chars = $chars.ToLower()
     }
 
+    # Loop through the amount of passwords to generate
     for ($i = 0; $i -lt $ListLength; $i++) {
+        # Declare variables
         $password = ""
+
+        # Loop through the length of the password
         for ($k = 0; $k -lt $Length; $k++) {
+            # Add a random character from the character list to the password
             $password += $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)]
         }
+
+        # Output the password
+        Write-Output $password
+    }
+}
+
+Function New-PasswordUsingWords {
+    <#
+    .SYNOPSIS
+    Generates a random password using words
+    
+    .DESCRIPTION
+    Generates a random password using words from a word list.
+    
+    .PARAMETER Words
+    The number of words to use in the password
+    
+    .PARAMETER ListLength
+    The number of passwords to generate
+    
+    .EXAMPLE
+    New-PasswordWords -Length 5 -ListLength 5
+    #>
+    Param(
+        [Parameter(Position = 0)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [int]$Words = 3,
+
+        [Parameter()]
+        [int]$ListLength = 1,
+
+        [Parameter()]
+        [string]$divider = "-"
+    )
+
+    # Load word list
+    $wordlist = Get-Content -Path "$PSScriptRoot\..\Data\wordlist.txt"
+
+    # Loop through the amount of passwords to generate
+    for ($i = 0; $i -lt $ListLength; $i++) {
+        # Declare variables
+        $password = ""
+        $wordsToUse = @()
+
+        # Loop through the amount of words to use
+        for ($k = 0; $wordsToUse.count -lt $Words; $k++) {
+            # Add a random word from the word list to the array
+            $wordsToUse += $wordlist[(Get-Random -Minimum 0 -Maximum $wordlist.Length)]
+        }
+        # Join the words together with the divider
+        $password = $wordsToUse -join $divider
+
+        # Output the password
         Write-Output $password
     }
 }
