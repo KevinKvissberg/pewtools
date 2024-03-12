e4079150b267d0a66e1a43e68a2ef5ebc848ce7b
Kevin Kvissberg <Kevin.Kvissberg@gmail.com>
2024-03-12 20:58:21 +0100

    Initial simple password generator

---
+++ pewtools/Public/generator-utils.ps1
@@ -0,0 +1,101 @@
+Function New-Password {
+    <#
+    .SYNOPSIS
+    Generates a random password
+    
+    .DESCRIPTION
+    Generates a random password with the specified length and constraints.
+    
+    .PARAMETER Length
+    The length of the password
+    
+    .PARAMETER NoSpecial
+    If set, the password will not contain special characters
+    
+    .PARAMETER NoNumbers
+    If set, the password will not contain numbers
+    
+    .PARAMETER OnlyNumbers
+    If set, the password will only contain numbers
+    
+    .PARAMETER OnlyLowercase
+    If set, the password will only contain lowercase letters
+    
+    .PARAMETER OnlyUppercase
+    If set, the password will only contain uppercase letters
+    
+    .PARAMETER AllowSimilarChars
+    If set, the password will contain similar characters (e.g. 1, l, I, 0, O, etc.)
+    
+    .PARAMETER ListLength
+    The number of passwords to generate
+    
+    .EXAMPLE
+    New-Password -Length 16 -NoSpecial -ListLength 5
+    #>
+    [CmdletBinding(DefaultParameterSetName = "AllChars")]
+    Param(
+        [Parameter(Position = 0)]
+        [int]$Length = 12,
+
+        [Parameter()]
+        [switch]$NoSpecial,
+        
+        [Parameter()]
+        [switch]$NoNumbers,
+        
+        [Parameter()]
+        [switch]$OnlyNumbers,
+        
+        [Parameter()]
+        [switch]$OnlyLowercase,
+        
+        [Parameter()]
+        [switch]$OnlyUppercase,
+
+        [Parameter()]
+        [switch]$AllowSimilarChars,
+
+        [Parameter()]
+        [int]$ListLength = 1
+    )
+
+    # Check constraints
+    if ($OnlyNumbers -and $NoNumbers) {
+        throw "OnlyNumbers and NoNumbers cannot be used together."
+    }
+    if ($OnlyLowercase -and $OnlyUppercase) {
+        throw "OnlyLowercase and OnlyUppercase cannot be used together."
+    }
+
+
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+/.,<>?;:[]{}-="
+
+    if ($NoSpecial) {
+        $chars = $chars -replace "[\W]", ""
+        $chars = $chars -replace "[-_]", ""
+    }
+    if (!$AllowSimilarChars) {
+        $chars = $chars -replace "[il1Lo0OvVwW]", ""
+    }
+    if ($NoNumbers) {
+        $chars = $chars -replace "[0-9]", ""
+    }
+    if ($OnlyNumbers) {
+        $chars = "0123456789"
+    }
+    elseif ($OnlyUppercase) {
+        $chars = $chars.ToUpper()
+    }
+    elseif ($OnlyLowercase) {
+        $chars = $chars.ToLower()
+    }
+
+    for ($i = 0; $i -lt $ListLength; $i++) {
+        $password = ""
+        for ($k = 0; $k -lt $Length; $k++) {
+            $password += $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)]
+        }
+        Write-Output $password
+    }
+}
